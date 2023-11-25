31486b1fa4bdb4f3d4375c6c58ae05f07fe9da3f
Yauheni Fiatsisau <dark0gf@gmail.com>
2023-11-25 01:32:24 +0100

    add cache folder

--- app/services/cryptoAnalysisTool.ts
+++ app/services/cryptoAnalysisTool.ts
@@ -26,9 +26,7 @@ type TListFuturesCandlesticksOpt = {
         // 'to': 1546935600, // number | End time of candlesticks, formatted in Unix timestamp in seconds. Default to current time
         'interval': "5m" // '10s' | '30s' | '1m' | '5m' | '15m' | '30m' | '1h' | '2h' | '4h' | '6h' | '8h' | '12h' | '1d' | '7d' | '1w' | '30d' | Interval time between data points. Note that `1w` means natual week(Mon-Sun), while `7d` means every 7d since unix 0.  Note that 30d means 1 natual month, not 30 days
     };
-    await futuresApi.listFuturesCandlesticks(settle, contracts[0].name || '', opts)
-        .then(value => console.log('API called successfully. Returned data: ', value.body),
-            error => console.error(error));
+    const {body: candlesticks} = await futuresApi.listFuturesCandlesticks(settle, contracts[0].name || '', opts);
 
 
 
