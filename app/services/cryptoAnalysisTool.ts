b2597f3675223c524edc68591eba144659c45a30
Yauheni Fiatsisau <dark0gf@gmail.com>
2023-12-04 23:27:28 +0100

    fetching candlesticks from gateio

--- app/services/cryptoAnalysisTool.ts
+++ app/services/cryptoAnalysisTool.ts
@@ -3,7 +3,8 @@ import {ApiClient, FuturesApi, FuturesCandlestick} from 'gate-api';
 import {writeFileSync} from "fs";
 
 const interval = "5m";
-const startBackFrom = 365 * 24 * 60 * 60;
+const startBackFrom = 365 * 24 * 60 * 60; //1 year
+const chunkTime = 5 * 24 * 60 * 60; //5 days
 
 type TListFuturesCandlesticksOpt = {
     from?: number;
@@ -15,9 +16,16 @@ type TListFuturesCandlesticksOpt = {
 const loadCandleStickCachedData = (cacheFileName: string) => {
     let futuresCandlesticksData: FuturesCandlestick[] = [];
     try {
-        let data = JSON.parse(readFileSync(cacheFileName).toString());
-    } catch (e) {}
+        const fileData = readFileSync(cacheFileName).toString();
+        futuresCandlesticksData = JSON.parse(fileData);
+    } catch (e) {
+        throw e;
+    }
     return futuresCandlesticksData;
+}
+
+const getCurrentTimestamp = () => {
+    return Math.round(Date.now() / 1000);
 }
 
 (async () => {
@@ -31,38 +39,36 @@ const loadCandleStickCachedData = (cacheFileName: string) => {
     const {body: contracts} = await futuresApi.listFuturesContracts(settle);
     console.log(contracts.length);
 
-
-    const opts: TListFuturesCandlesticksOpt = {
-        // 'from': 1546905600, // number | Start time of candlesticks, formatted in Unix timestamp in seconds. Default to`to - 100 * interval` if not specified
-        // 'to': 1546935600, // number | End time of candlesticks, formatted in Unix timestamp in seconds. Default to current time
-        'interval': interval // '10s' | '30s' | '1m' | '5m' | '15m' | '30m' | '1h' | '2h' | '4h' | '6h' | '8h' | '12h' | '1d' | '7d' | '1w' | '30d' | Interval time between data points. Note that `1w` means natual week(Mon-Sun), while `7d` means every 7d since unix 0.  Note that 30d means 1 natual month, not 30 days
-    };
     console.log('contract', contracts[0]);
-    const {body: candlesticks} = await futuresApi.listFuturesCandlesticks(settle, contracts[0].name || '', opts);
-
-    console.log('candle', candlesticks[0]);
 
     const cacheFileName = `./cache/gateio-${contracts[0].name}@${interval}`;
     const futuresCandlesticksData = loadCandleStickCachedData(cacheFileName);
 
-    let lastTimestamp = futuresCandlesticksData.length > 0 ? futuresCandlesticksData[futuresCandlesticksData.length - 1] : Math.round(Date.now() / 1000) - startBackFrom;
-    console.log(futuresCandlesticksData);
 
+    let lastTimestamp = futuresCandlesticksData.length > 0 ?
+        futuresCandlesticksData[futuresCandlesticksData.length - 1].t || 0 :
+        getCurrentTimestamp() - startBackFrom;
 
+    console.log(futuresCandlesticksData[futuresCandlesticksData.length - 1]);
 
-    writeFileSync(cacheFileName, JSON.stringify(futuresCandlesticksData));
+    console.log('last candle time', new Date(lastTimestamp * 1000));
 
+    while (lastTimestamp - chunkTime < getCurrentTimestamp()) {
+        const from = lastTimestamp + 1;
+        const to = lastTimestamp + chunkTime;
+        console.log('getting from to', new Date(from * 1000), new Date(to * 1000));
+        const opts: TListFuturesCandlesticksOpt = {
+            'from': from,
+            'to': to,
+            'interval': interval
+        };
+        const {body: candlesticks} = await futuresApi.listFuturesCandlesticks(settle, contracts[0].name || '', opts);
+        futuresCandlesticksData.push(...candlesticks);
+        writeFileSync(cacheFileName, JSON.stringify(futuresCandlesticksData));
 
-// const contract = pair; // string | Futures contract
-// const opts: TListFuturesCandlesticksOpt = {
-//     'from': 1546905600, // number | Start time of candlesticks, formatted in Unix timestamp in seconds. Default to`to - 100 * interval` if not specified
-//     'to': 1546935600, // number | End time of candlesticks, formatted in Unix timestamp in seconds. Default to current time
-//     'interval': "5m" // '10s' | '30s' | '1m' | '5m' | '15m' | '30m' | '1h' | '2h' | '4h' | '6h' | '8h' | '12h' | '1d' | '7d' | '1w' | '30d' | Interval time between data points. Note that `1w` means natual week(Mon-Sun), while `7d` means every 7d since unix 0.  Note that 30d means 1 natual month, not 30 days
-// };
-// api.listFuturesCandlesticks(settle, contract, opts)
-//     .then(value => console.log('API called successfully. Returned data: ', value.body),
-//         error => console.error(error));
-
+        lastTimestamp += chunkTime;
+    }
+    console.log(futuresCandlesticksData.length);
 
 
 })();
