f8b2199328a90a9c539f253f89d93efa888f43d6
Yauheni Fiatsisau <dark0gf@gmail.com>
2024-01-20 13:59:30 +0100

    add async load for ploty and test plot

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,8 +1,26 @@
+"use client"
+import React, {useEffect, useState} from 'react';
 
 export default function Analysis() {
+    const [Chart, setChart] = useState<any>();
+
+    useEffect(() => {
+        (async () => {
+            const {default: Chart} = await import('./Chart');
+
+            const TestChart = function TestChart() {
+                return <>Hi!</>
+            }
+            console.log(<Chart />);
+            console.log(<TestChart />);
+            setChart(<Chart />);
+        })();
+
+    }, []);
+
     return (
         <main className="h-full text-black">
-            TEST
+            {Chart}
         </main>
     )
 }
